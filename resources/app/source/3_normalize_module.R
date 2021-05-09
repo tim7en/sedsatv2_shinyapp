bfcef06eaef05891f3c465d54e611265092f4aff
Timur Sabitov <sabitov.ty@gmail.com>
2021-05-09 04:12:26 -0400

    upd 3

--- resources/app/source/3_normalize_module.R
+++ resources/app/source/3_normalize_module.R
@@ -19,11 +19,11 @@ normalize_ui <- function(id) {
             sidebarPanel(
               uiOutput(ns("shapiroP")),
               uiOutput(ns("spPlotpick"))
-<<<<<<< HEAD
+#<<<<<<< HEAD
               ,span(textOutput(ns("descriptionOut3")))
-=======
+#=======
               #,span(textOutput(ns("descriptionOut3")))
->>>>>>> c93056dc7f903a36381b7918469f2293df53c9b0
+#>>>>>>> c93056dc7f903a36381b7918469f2293df53c9b0
             ), width = 4
           ),
           mainPanel(
